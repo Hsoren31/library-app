04e3d5e3a6328d08b7e662de5d60601c0288e7e9
Haley Sorensen <sorensenhaley@gmail.com>
2024-03-21 17:08:17 -0600

    Add a new book form for user to add book and submit to display on page

--- script.js
+++ script.js
@@ -32,9 +32,8 @@ function addBookToLibrary(book) {
 //Make cards to display books to user
 const libraryContainer = document.getElementById('library_container');
 
-function makeCard(library){
+function displayLibrary(library){
     for (let book of library){
-        console.log(book.title)
         const cardBook = document.createElement('div');
         cardBook.className = 'card';
         libraryContainer.appendChild(cardBook);
@@ -60,5 +59,58 @@ function makeCard(library){
         cardBook.appendChild(ifRead);
     }
 }
+displayLibrary(library);
 
-makeCard(library);
+//Add single book card to display
+function makeCard(Book){
+    const cardBook = document.createElement('div');
+    cardBook.className = 'card';
+    libraryContainer.appendChild(cardBook);
+
+    const title = document.createElement('h1');
+    title.className = 'title';
+    title.textContent = Book.title;
+    cardBook.appendChild(title);
+
+    const author = document.createElement('p');
+    author.className = 'author';
+    author.textContent = Book.author;
+    cardBook.appendChild(author);
+
+    const pages = document.createElement('p');
+    pages.className = 'pages';
+    pages.textContent = Book.pages;
+    cardBook.appendChild(pages);
+
+    const ifRead = document.createElement('p');
+    ifRead.className = 'if_read';
+    ifRead.textContent = Book.ifRead;
+    cardBook.appendChild(ifRead);
+}
+
+// const book1 = new Book ('book', 'author', 100, 'have read');
+// console.log(book1);
+// makeCard(book1);
+
+//Get add book form to display
+let newBook = document.getElementById('new_book');
+
+newBook.addEventListener('click', function(){
+    document.getElementById('pop_up_form').style.display = "block";
+})
+
+//Form submit button
+let newBookForm = document.getElementById('add_book_form');
+
+newBookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let newTitle = document.querySelector('#title').value;
+    let newAuthor = document.querySelector('#author').value;
+    let newPages = document.querySelector('#pages').value;
+    let newHaveRead = document.querySelector('#have_read').value;
+    let newBook = new Book(newTitle, newAuthor, newPages, newHaveRead);
+    addBookToLibrary(newBook);
+    console.log(library)
+    makeCard(newBook);
+});
