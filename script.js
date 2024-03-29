19b82746a7247438eec123fe0e19d464d02370a2
Haley Sorensen <sorensenhaley@gmail.com>
2024-03-29 10:34:14 -0600

    Add form to dialog to open and close

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const showForm = document.getElementById('new_book');
 const closeButton = document.getElementsByClassName('close_form');
 const submitButton = document.getElementById('submit_book');
 
+//Display and close form
+showForm.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+closeButton.addEventListener('click', () => {
+    dialog.close();
+});
